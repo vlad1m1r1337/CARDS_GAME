1dbe03b67e892f64df25a86adef3a3d6f5fe5ff3
vladim1r1337 <vladim1r.gribkov@yandex.ru>
2023-07-20 14:20:34 +0400

    rdy test

--- MyGame/src/random_card.test.ts
+++ MyGame/src/random_card.test.ts
@@ -1,16 +1,60 @@
-import {assert} from "assert";
+//-----------------------------------------------------------------------------------------------------
 
-//const assert = require("assert").strict;
 import { randomCards } from "./random_card";
-//const { randomCards } = require("./random_card");
-
 {
 	// Подготовка
 	const expected = 6;
 
 	// Действие
-	const is = randomCards.length;
+	const is = randomCards(3).length;
 
-	//Сверка
-	assert.equal(is, expected);
+	if (expected !== is){
+		console.log(`Expected ${expected}, but got ${is}`);
+	}
+	else
+		console.log("First test is successful");
 }
+{
+	// Подготовка
+	const expected = 12;
+
+	// Действие
+	const is = randomCards(6).length;
+
+	if (expected !== is){
+		console.log(`Expected ${expected}, but got ${is}`);
+	}
+	else
+		console.log("Second test is successful");
+}
+{
+	// Подготовка
+	const expected = 36;
+
+	// Действие
+	const is = randomCards(18).length;
+
+	if (expected !== is){
+		console.log(`Expected ${expected}, but got ${is}`);
+	}
+	else
+		console.log("Third test is successful");
+}
+
+//-----------------------------------------------------------------------------------------------------
+
+// Не получилось подключить assert
+
+// import assert from 	;
+// import { randomCards } from "./random_card";
+
+// {
+//     // Подготовка
+//     const expected = 6;
+
+//     // Действие
+//     const is = randomCards(3).length;
+
+//     // Сравнение
+//     assert.strictEqual(expected, is, `Expected ${expected}, but got ${is}`);
+// }
